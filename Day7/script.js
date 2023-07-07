82e2314332b3bf86a72970a90c1e404679146157
Jeeva1302 <jeevanandham.kanini@gmail.com>
2023-07-07 15:32:40 +0530

    day 7 task added

---
+++ Day7/script.js
@@ -0,0 +1,10 @@
+function f1()
+{
+    const x=2, y=3;
+    document.getElementById("para1").innerHTML=x+y;
+    {
+        let x=5;
+        document.getElementById("para2").innerHTML=x+y
+    }
+
+}
